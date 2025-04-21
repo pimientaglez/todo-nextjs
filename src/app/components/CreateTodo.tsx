184f7841fb608df1a59eefe0dce8ed4ba2fbc6b1
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-20 23:32:54 -0600

    Create Todo form

---
+++ src/app/components/CreateTodo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreateTodo = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="title" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTodo;
