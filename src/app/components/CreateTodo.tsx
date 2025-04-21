9ae8b9d4a48b5ca540fa821acbc41f2ec4c078c6
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-20 23:49:25 -0600

    Add logic to create todos

--- src/app/components/CreateTodo.tsx
+++ src/app/components/CreateTodo.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (title.length > 0) {
+      createTodo(title);
+      setTitle("");
+    }
+  };
+  const createTodo = async (title: string) => {
+    await fetch("http://localhost:8080/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: title, status: "PENDING" }),
+    });
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" name="title" />
+      <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+        <input
+          type="text"
+          name="title"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleInputChange(e)
+          }
+          value={title}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
