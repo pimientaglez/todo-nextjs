ca5784a0425200d7542aaca85bf20369a46e3786
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-23 14:50:43 -0600

    Add basic tailwind styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import MainContainer from "./components/MainContainer";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20">
-      <h1>ToDo App</h1>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 bg-slate-950">
+      <h1 className="font-mono font-medium text-5xl align-middle mb-2">
+        ToDo App
+      </h1>
       <MainContainer />
     </div>
   );
