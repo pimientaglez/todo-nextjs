a04a4baf679c6bb3aab2fde93779aeb92e843a45
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-26 18:19:18 -0600

    Update bg-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import MainContainer from "./components/MainContainer";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 bg-slate-950">
-      <h1 className="font-mono font-medium text-5xl align-middle mb-2">
-        ToDo App
-      </h1>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 bg-blue-900">
+      <h1 className="font-mono font-medium text-5xl align-middle mb-2"></h1>
       <MainContainer />
     </div>
   );
