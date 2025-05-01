8a1c5bbe5c90088d691fcc282b124020036690ce
Ricardo Pimienta <pimientaglez@gmail.com>
2025-05-01 12:04:47 -0600

    Add a Dialog window for creating a Todo

--- src/app/components/CreateTodo.tsx
+++ src/app/components/CreateTodo.tsx
@@ -2,16 +2,29 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Pen } from "lucide-react";
+import { Label } from "@/components/ui/label";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
 
 interface CreateTodoProps {
   onTodoCreated: () => void;
 }
 const CreateTodo: React.FC<CreateTodoProps> = ({ onTodoCreated }) => {
   const [title, setTitle] = useState("");
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  /* const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
-  };
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  }; */
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     if (title.length > 0) {
       await createTodo(title);
@@ -30,26 +43,46 @@ const CreateTodo: React.FC<CreateTodoProps> = ({ onTodoCreated }) => {
   };
 
   return (
-    <div>
-      <form
-        className="flex mb-2"
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
-      >
-        <Input
-          type="text"
-          name="title"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleInputChange(e)
-          }
-          value={title}
-          className="mr-2"
-        />
+    <Dialog>
+      <DialogTrigger asChild>
         <Button variant="outline" className="cursor-pointer">
           <Pen className="mr-2 h-4 w-4" aria-hidden="true" />
           Add Todo
         </Button>
-      </form>
-    </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Todo</DialogTitle>
+          <DialogDescription>
+            Create a new Todo. Click save when you´re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button
+            type="submit"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              handleSubmit(e)
+            }
+          >
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
